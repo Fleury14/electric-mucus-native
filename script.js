2f786ef9e1ac4d2e8bcedeafca013483f3fbc85f
Josh R. Dunlavy <jdunlavy@gmail.com>
2018-05-07 14:54:17 -0700

    solution

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
+const button = window.document.getElementById("button-id");
+const input = window.document.getElementById("input-id");
 
+button.addEventListener("click", () => {
+	button.disabled = true;
+	input.disabled = true;
+	alert("Done!");
+});
+
+input.addEventListener("input", event => {
+	const correctAnswer = "electric mucus";
+	const userInput = event.target.value.toLowerCase();
+
+	button.style.backgroundColor = userInput === correctAnswer ? "green" : userInput;
+	button.disabled = userInput !== correctAnswer;
+});
